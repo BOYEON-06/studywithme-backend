039f2cd5201dae688798f74cbca4772e7e2bb984
hyeju0116 <cholhyeju123@gmail.com>
2026-05-26 20:51:17 +0900

    Update WebSocketConfig.java

--- StudyWithMe/src/main/java/com/example/StudyWithMe/config/WebSocketConfig.java
+++ StudyWithMe/src/main/java/com/example/StudyWithMe/config/WebSocketConfig.java
@@ -24,6 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트가 소켓에 연결할 엔드포인트
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
